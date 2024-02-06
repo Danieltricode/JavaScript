a6519a6e8127affa9284b590d33d6d1d0cec88cd
Daniel Trindade <147204744+danieltrin@users.noreply.github.com>
2024-02-06 17:57:47 -0300

    Novos arquivos e pasta sobre condições

---
+++ exercicios/condições/condicao001.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A velociade de seu carro é ${vel}Km/m`)
+if ( vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida, você será multado!')
+}
+// Pressione f8
