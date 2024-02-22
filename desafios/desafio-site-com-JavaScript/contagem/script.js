2e7a3628a6c51d2d4e58f19156f713e3dbdd33b5
Daniel Trindade <147204744+danieltrindade3@users.noreply.github.com>
2024-02-22 18:19:02 -0300

    Criação de novos desafios.

---
+++ desafios/desafio-site-com-JavaScript/contagem/script.js
@@ -0,0 +1,6 @@
+function contar() {
+    var ini = document.getElementById('ini')
+    var fim = document.getElementById('fim')
+    var pass = document.getElementById('pass')
+    var cont = document.getElementById('cont')
+}
