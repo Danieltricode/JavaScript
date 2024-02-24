6d347c9ae2a631bedccb75e14c4ecbb9cb25dcc0
Daniel Trindade <147204744+danieltrindade3@users.noreply.github.com>
2024-02-24 15:33:05 -0300

    Desafio do contador concluído!

--- desafios/desafio-site-com-JavaScript/contagem/script.js
+++ desafios/desafio-site-com-JavaScript/contagem/script.js
@@ -1,6 +1,27 @@
 function contar() {
-    var ini = document.getElementById('ini')
-    var fim = document.getElementById('fim')
-    var pass = document.getElementById('pass')
+    let ini = document.getElementById('ini')
+    let fim = document.getElementById('fim')
+    let pass = document.getElementById('pass')
     var cont = document.getElementById('cont')
+    if (ini.value.lemgth == 0 || fim.value.length == 0 || pass.value.length == 0) {
+        cont.innerHTML = "Impossível contar!"
+    } else {
+        cont.innerHTML = "Contando: <br>"
+        i = Number(ini.value)
+        f = Number(fim.value)
+        p = Number(pass.value)
+        if (p <= 0) {
+            cont.innerHTML = "Passo inválido considere 1 passo!"
+        }
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+            cont.innerHTML += ` ${c} \u{1F449}`
+             }
+        } else {
+            for (let c = i; c >= f; c -= p) {
+                cont.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        cont.innerHTML += `\u{1F3C1}`
+    }
 }
