78850f1798fe8aba071251cd518aaeb20136034e
Daniel Trindade <147204744+danieltrindade3@users.noreply.github.com>
2024-03-06 11:11:00 -0300

    Novos exercícios de função e mudanças na pasta "exercicios".

---
+++ exercicios/function-basic/function001.js
@@ -0,0 +1,9 @@
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Ímpar!'
+    }
+}
+console.log(parimpar(123))
+// Revela se um número é par ou ímpar.
