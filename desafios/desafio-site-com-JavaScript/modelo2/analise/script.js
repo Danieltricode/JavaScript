e89e0873a6f3e14fa6e5bd375e363d1f29ede559
Daniel Trindade <147204744+danieltrindade3@users.noreply.github.com>
2024-03-09 18:52:36 -0300

    desafio com novo modelo criado.

---
+++ desafios/desafio-site-com-JavaScript/modelo2/analise/script.js
@@ -0,0 +1,9 @@
+function adicionar() {
+    let an = document.getElementById('txtnum')
+    let tab = document.getElementById('tab')
+    if (an.value.length == 0 || an.value > 100) {
+        window.alert('Erro, adiciona um número!')
+    } else {
+         
+    }
+}
