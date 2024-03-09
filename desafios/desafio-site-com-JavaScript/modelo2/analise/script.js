143e67e4fcab559d2d2a828e4670518c58a6649a
Daniel Trindade <147204744+danieltrindade3@users.noreply.github.com>
2024-03-09 20:27:19 -0300

    Script do dasafio analisador de números em andamento.

--- desafios/desafio-site-com-JavaScript/modelo2/analise/script.js
+++ desafios/desafio-site-com-JavaScript/modelo2/analise/script.js
@@ -4,6 +4,12 @@ function adicionar() {
     if (an.value.length == 0 || an.value > 100) {
         window.alert('Erro, adiciona um número!')
     } else {
-         
+        var num = [an.value]
+        var item = document.createElement('option')
+        item.text = `Valor ${num}`
+        tab.appendChild(item)
     }
+}
+function finalizar() {
+    
 }
