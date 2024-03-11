f27f2a2c725cae8e25a8ceb14aa4c771510ba54d
Daniel Trindade <147204744+danieltrindade3@users.noreply.github.com>
2024-03-11 20:15:30 -0300

    Function "adicionar" terminada

--- desafios/desafio-site-com-JavaScript/modelo2/analise/script.js
+++ desafios/desafio-site-com-JavaScript/modelo2/analise/script.js
@@ -10,7 +10,7 @@ function isNumero(n) {
      }
 }
 function inLista(n, l) {
-    if (l.indexOf(number(n)) != -1) {
+    if (l.indexOf(Number(n)) != -1) {
         return true
     } else {
         return false
@@ -18,9 +18,24 @@ function inLista(n, l) {
 }
 
 function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value)) {
-        
+    if(isNumero(num.value) && !inLista(num.value, valor)) {
+        valor.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
         window.alert('Valor inválido ou já encontrado na lista.')
     }
+    num.value = ''
+    num.focus()
+}
+function finalizar() {
+    valor.sort
+    let text = document.createElement('p')
+    res.appendChild(text)
+    text.innerText = `Ao todo temos ${valor.length} números.
+    O maior valor foi.
+    O menor valor foi ${valor[0]}.
+    Somando todos os valores, temos.
+    A média dos valores digitados é.`
 }
